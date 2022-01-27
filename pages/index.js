68846e8ac8b9421fe5bc0d949ab45ff85ae302f1
Vinícius Gabriel <viniciusgabriel.rs@hotmail.com>
2022-01-26 23:58:02 -0300

    aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
 import appConfig from "../config.json";
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
-
+import React from "react";
+import {useRouter} from "next/router";
 
 function Title(props) {
     const Tag = props.tag || h1;
@@ -49,11 +21,11 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-    const username = 'viniciusgabriels';
+    const [username, setUsername] = React.useState('viniciusgabriels');
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -80,6 +52,10 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                roteamento.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -91,6 +67,11 @@ export default function PaginaInicial() {
               </Text>
   
               <TextField
+                value={username}
+                onChange={function(event) {
+                  const valor = event.target.value;
+                  setUsername(valor);
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
